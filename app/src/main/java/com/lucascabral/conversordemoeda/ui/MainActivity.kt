1c045513c9f47060ceb7341e8646ff0e8c45f773
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-07-16 17:27:05 -0300

    Your current Subscription Plan does not support this API Function.

--- app/src/main/java/com/lucascabral/conversordemoeda/ui/MainActivity.kt
+++ app/src/main/java/com/lucascabral/conversordemoeda/ui/MainActivity.kt
@@ -1,16 +1,53 @@
 package com.lucascabral.conversordemoeda.ui
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import com.lucascabral.conversordemoeda.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.convertButton.setOnClickListener {
+            viewModel.convert(
+                binding.fromTextInputEditText.text.toString(),
+                binding.fromCurrencySpinner.selectedItem.toString(),
+                binding.toCurrencySpinner.selectedItem.toString()
+            )
+        }
+
+        lifecycleScope.launchWhenCreated {
+            viewModel.conversion.collect { event ->
+                when(event) {
+                    is MainViewModel.CurrencyEvent.Success -> {
+                        binding.progressBar.isVisible = false
+                        binding.resultTextView.setTextColor(Color.BLACK)
+                        binding.resultTextView.text = event.resultText
+                    }
+                    is MainViewModel.CurrencyEvent.Failure -> {
+                        binding.progressBar.isVisible = false
+                        binding.resultTextView.setTextColor(Color.RED)
+                        binding.resultTextView.text = event.errorText
+                    }
+                    is MainViewModel.CurrencyEvent.Loading -> {
+                        binding.progressBar.isVisible = true
+                    }
+                    else -> Unit
+                }
+            }
+        }
     }
 }
